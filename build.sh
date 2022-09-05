8980c6d9dff955d6fe90021105c45e7d20ba10b8
vyrjana <41105805+vyrjana@users.noreply.github.com>
2022-09-05 09:09:51 +0300

    Updated build script

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
+if [ "$?" -ne 0 ]; then
+	echo "Found issues that will cause the automated tests to fail!"
+	exit
+fi
 # Build wheel
 python3 -m build
