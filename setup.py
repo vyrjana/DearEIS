f4cffd803f40d589bb6a8b95bc3d33d2e0ec84e0
vyrjana <41105805+vyrjana@users.noreply.github.com>
2024-07-16 07:33:32 +0300

    Updated remaining development dependencies

--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ dependencies = [
 ]
 
 dev_dependencies = [
-    "build~=1.1",
-    "flake8~=6.0",
+    "build~=1.2",
+    "flake8~=7.0",
     "setuptools~=70.0",
-    "sphinx~=7.2",
+    "sphinx~=7.3",
     "sphinx-rtd-theme~=2.0",
 ]
 
